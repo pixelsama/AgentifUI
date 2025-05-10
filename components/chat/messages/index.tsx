5b3c1ad01d1d434d9e5c0b7fbebe34ef527d97f2
lyzno1 <yuanyouhuilyz@gmail.com>
2025-05-10 16:16:22 +0800

    fix: 修复消息组件的移动错误

--- components/chat/messages/index.tsx
+++ components/chat/messages/index.tsx
@@ -1,8 +1,8 @@
 import { UserMessage } from './user-message/user-message';
-import { AssistantMessage } from './user-message/assistant-message';
+import { AssistantMessage } from './assistant-message/assistant-message';
 import { FileAttachmentDisplay } from './user-message/file-attachment-display';
 import { UserMessageActions } from './user-message/user-message-actions';
-import { AssistantMessageActions } from './user-message/assistant-message-actions';
+import { AssistantMessageActions } from './assistant-message/assistant-message-actions';
 
 export { 
   UserMessage, 
